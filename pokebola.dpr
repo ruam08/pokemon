091332d4d2c633a91ebbd84744e1c46b4f9fd62b
ruam08 <ruanalvesbarbosa@gmail.com>
2022-05-24 16:30:49 -0300

    Commit inicial

---
+++ pokebola.dpr
@@ -0,0 +1,14 @@
+program pokebola;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  uPrincipal in 'uPrincipal.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
---
+++ uPrincipal.pas
@@ -0,0 +1,125 @@
+unit uPrincipal;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.ListView.Types, FMX.ListView.Appearances, FMX.ListView.Adapters.Base,
+  FMX.ListView, FMX.StdCtrls, System.json, IdIOHandler, IdIOHandlerSocket,
+  IdIOHandlerStack, IdSSL, IdSSLOpenSSL, IdBaseComponent, IdComponent,
+  IdTCPConnection, IdTCPClient, IdHTTP, System.ImageList, FMX.ImgList;
+
+type
+  TfrmPrincipal = class(TForm)
+    ListView1: TListView;
+    AniIndicator1: TAniIndicator;
+    procedure FormShow(Sender: TObject);
+  private
+    { Private declarations }
+    function GetUrlJsonArray(url: string): TJSONArray;
+    function DownloadIMG(url: string): TStream;
+    procedure TratamentoListagem(Sender: TObject);
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPrincipal: TfrmPrincipal;
+
+const
+  API_URL = 'https://pokeapi.glitch.me/v1/pokemon/6';
+
+implementation
+
+{$R *.fmx}
+
+procedure TfrmPrincipal.FormShow(Sender: TObject);
+var
+  LoadPokebola: TThread;
+  JsonArrayPokemon: TJSONArray;
+  JsonPokemon: TJSONObject;
+  Sprinte: TStream;
+begin
+  AniIndicator1.Enabled := true;
+  AniIndicator1.Visible := true;
+  LoadPokebola := TThread.CreateAnonymousThread(
+    procedure
+    var
+      I: integer;
+      LItem: TListViewItem;
+    begin
+      JsonArrayPokemon := GetUrlJsonArray(API_URL);
+
+      for I := 0 to JsonArrayPokemon.Size - 1 do
+      begin
+        JsonPokemon := JsonArrayPokemon.Get(I) as TJSONObject;
+        Sprinte := DownloadIMG(JsonPokemon.GetValue('sprite').Value);
+        TThread.Synchronize(TThread.CurrentThread,
+          procedure
+          begin
+            frmPrincipal.ListView1.BeginUpdate;
+            LItem := frmPrincipal.ListView1.Items.Add;
+            LItem.Bitmap.LoadFromStream(Sprinte);
+            LItem.Text := JsonPokemon.GetValue('name').Value;
+            LItem.Detail := JsonPokemon.GetValue('name').Value;
+            frmPrincipal.ListView1.EndUpdate;
+          end);
+      end;
+    end);
+  LoadPokebola.OnTerminate := TratamentoListagem;
+  LoadPokebola.start();
+end;
+
+function TfrmPrincipal.GetUrlJsonArray(url: string): TJSONArray;
+var
+  HTTP: TIdHTTP;
+begin
+  HTTP := TIdHTTP.Create(nil);
+  try
+    HTTP.IOHandler := TIdSSLIOHandlerSocketOpenSSL.Create(HTTP);
+    result := TJSONObject.ParseJSONValue(TEncoding.UTF8.GetBytes(HTTP.Get(url)),
+      0) as TJSONArray;
+  finally
+    FreeAndNil(HTTP);
+  end;
+end;
+
+procedure TfrmPrincipal.TratamentoListagem(Sender: TObject);
+begin
+  if ((Sender as TThread).FatalException = nil) then
+  begin
+    AniIndicator1.Visible := false;
+  end
+  else
+  begin
+    AniIndicator1.Visible := false;
+    ShowMessage('Falha ao conultar a lista de Pokemons, erro:' + ((Sender as TThread).FatalException as Exception).Message);
+  end;
+end;
+
+function TfrmPrincipal.DownloadIMG(url: string): TStream;
+var
+  HTTP: TIdHTTP;
+  IMG: TMemoryStream;
+begin
+  HTTP := TIdHTTP.Create(nil);
+  try
+    try
+      IMG := TMemoryStream.Create;
+      HTTP.Request.UserAgent :=
+        'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36';
+      HTTP.HandleRedirects := true;
+      HTTP.IOHandler := TIdSSLIOHandlerSocketOpenSSL.Create(HTTP);
+      HTTP.Get(url, IMG);
+      result := IMG;
+    except
+
+    end;
+  finally
+    FreeAndNil(HTTP);
+  end;
+end;
+
+end.
